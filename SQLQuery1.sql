31b329872b72b70eb3b684d8a74312714e96d0da
Trupti Makwana <149158750+m-trupti@users.noreply.github.com>
2025-02-25 13:09:07 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Products (
+    ProductId INT IDENTITY(1,1) PRIMARY KEY,
+    ProductName NVARCHAR(100) NOT NULL,
+    Description NVARCHAR(500),
+    Price DECIMAL(18, 2),
+    ImageUrl NVARCHAR(255)
+);
---
+++ dbo.Products.data.sql
@@ -0,0 +1,17 @@
+﻿SET IDENTITY_INSERT [dbo].[Products] ON
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (1, N'Sofa1', N'First Sofa', CAST(45000.00 AS Decimal(18, 2)), N'..\Images\prodimages\sofa1.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (2, N'Sofa2', N'sofasecond ', CAST(55000.00 AS Decimal(18, 2)), N'..\Images\prodimages\sofa2.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (3, N'bed1', N' Maple Prime Bed With Semi Hydraulic Storage (White)', CAST(70000.00 AS Decimal(18, 2)), N'..\Images\prodimages\s\sofa2.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (4, N'bed2 ', N'Zion Max Bed With Storage (Walnut)', CAST(65000.00 AS Decimal(18, 2)), N'..\Images\prodimages\s\bed1.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (5, N'chair1', N' Illusion Plastic Arm Chair', CAST(1500.00 AS Decimal(18, 2)), N'..\Images\prodimages\s\chair1.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (6, N'chair2', N'Tulip Plastic Arm Chair', CAST(1200.00 AS Decimal(18, 2)), N'..\Images\prodimages\s\chair2.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (7, N'sidetable1', N'Center Table 5 (Marble Beige)', CAST(1000.00 AS Decimal(18, 2)), N'..\Images\prodimages\s\sidetable1.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (8, N'sidetable2', N'Centro Table 02 (Pear Wood)', CAST(800.00 AS Decimal(18, 2)), N'..\Images\prodimages\s\sidetable2.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (9, N'wardrobe1', N' Crystal 2 Door Wardrobe (New Wenge & Natural Ebony)', CAST(26000.00 AS Decimal(18, 2)), N'..\Images\prodimages\s\wardrobe1.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (10, N'chair1', N' Illusion Plastic Arm Chair', CAST(1500.00 AS Decimal(18, 2)), N'..\Images\prodimages\s\chair1.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (11, N'chair2', N'Tulip Plastic Arm Chair', CAST(1200.00 AS Decimal(18, 2)), N'..\Images\prodimages\s\chair2.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (12, N'sidetable1', N'Center Table 5 (Marble Beige)', CAST(1000.00 AS Decimal(18, 2)), N'..\Images\prodimages\s\sidetable1.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (13, N'sidetable2', N'Centro Table 02 (Pear Wood)', CAST(800.00 AS Decimal(18, 2)), N'..\Images\prodimages\s\sidetable2.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (14, N'wardrobe1', N' Crystal 2 Door Wardrobe (New Wenge & Natural Ebony)', CAST(26000.00 AS Decimal(18, 2)), N'..\Images\prodimages\s\wardrobe1.png')
+INSERT INTO [dbo].[Products] ([ProductId], [ProductName], [Description], [Price], [ImageUrl]) VALUES (15, N'wardrobe2', N' Eternal 3 Door Wardrobe with Mirror (Teak Finish)', CAST(32000.00 AS Decimal(18, 2)), N'..\Images\prodimages\s\wardrobe2.png')
+SET IDENTITY_INSERT [dbo].[Products] OFF
